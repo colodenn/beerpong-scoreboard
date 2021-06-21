c982fd955f3a9ed0c17f1a6f65072b349ff32778
Cornelius Denninger <cornelius.denninger@gmail.com>
2021-06-21 15:43:01 +0200

    fixed responsive bug

--- components/table.js
+++ components/table.js
@@ -1,6 +1,7 @@
 import {Table, Thead, Tbody, Tr, Th, Td} from 'react-super-responsive-table'
 
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
+
 const Tables = (props) => {
 
     const columns = props.columns
@@ -12,14 +13,14 @@ const Tables = (props) => {
     
   return (
     <>
-    <Table  style={{"backgroundColor": "#FFA3B3"}} className="border-black border text-left  " >
+    <Table  style={{"backgroundColor": "#FFA3B3"}} className="border-black border text-left   " >
        <Thead className="">
   
            <Tr  className="bg-black text-white py-4">
           {
             columns.map((e) => {
               return (
-               <Th className="p-4">
+               <Th className="py-2">
                  {e.Header}
                </Th>
               )
@@ -30,17 +31,16 @@ const Tables = (props) => {
   
        </Thead>
        <Tbody className="border border-black h-80 overflow-y-scroll ">
-      
           {data.map((e) => { 
             {console.log(e)}
             return (
-            <Tr  className="border border-black">
+            <Tr  className="border border-black " style={{"backgroundColor": "#FFC8D1"}}>
              
         
   {
     arr.map(t => {
         return (
-          <Td className="border border-black p-4 sm:p-2" style={{"backgroundColor":"#FFC8D1"}}>
+          <Td className="border border-black sm:py-2 py-0 " style={{"backgroundColor":"#FFC8D1"}}>
               {e[t]}
             </Td>
         )
@@ -60,6 +60,44 @@ const Tables = (props) => {
        </Tbody>
      </Table>
     
+     {/* <Table>
+  <Thead>
+    <Tr>
+      <Th>Event</Th>
+      <Th>Date</Th>
+      <Th>Location</Th>
+      <Th>Organizer</Th>
+      <Th>Theme</Th>
+      <Th>Agent</Th>
+    </Tr>
+  </Thead>
+  <Tbody>
+    <Tr>
+      <Td>Tablescon</Td>
+      <Td>9 April 2019</Td>
+      <Td>East Annex</Td>
+      <Td>Super Friends</Td>
+      <Td>Data Tables</Td>
+      <Td>Coston Perkins</Td>
+    </Tr>
+    <Tr>
+      <Td>Capstone Data</Td>
+      <Td>19 May 2019</Td>
+      <Td>205 Gorgas</Td>
+      <Td>Data Underground</Td>
+      <Td>Data Scence</Td>
+      <Td>Jason Phillips</Td>
+    </Tr>
+    <Tr>
+      <Td>Tuscaloosa D3</Td>
+      <Td>29 June 2019</Td>
+      <Td>Github</Td>
+      <Td>The Contributors Consortium</Td>
+      <Td>Data Viz</Td>
+      <Td>Coston Perkins</Td>
+    </Tr>
+  </Tbody>
+</Table> */}
      </>
   )
 }
