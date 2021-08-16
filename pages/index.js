d1f7c96fee5e58eaf8fbb79e47d515183ecb2381
Cornelius Denninger <cornelius.denninger@gmail.com>
2021-08-16 20:01:05 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <div className="sm:p-10 container justify-center mx-auto p-4  min-h-screen">
       <Tabs >
-        <h1 className="text-black font-semibold text-3xl mb-8 sm:text-left text-center ">🍻 Beerpong gedöns und so</h1>
+        <h1 className="text-black font-semibold text-3xl mb-8 sm:text-left text-center ">🍻 Beerpong Gedöns und so</h1>
           <TabList className="flex  justify-center sm:justify-start mb-4">
             <Tab className="text-white font-normal sm:text-2xl text-base mr-4 cursor-pointer">Solo</Tab>
             <Tab className="text-white font-normal sm:text-2xl text-base mr-4 cursor-pointer">Team</Tab>
